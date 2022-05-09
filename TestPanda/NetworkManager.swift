fc81a157ef08a9b8c6275a4968b37c68d0f80034
Sergey Shcheglov <97shcheglov@gmail.com>
2022-05-09 16:25:40 +0600

    Add network manager class

---
+++ TestPanda/NetworkManager.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkManager.swift
+//  TestPanda
+//
+//  Created by Sergey Shcheglov on 09.05.2022.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+    private init() {
+        
+    }
+    private let baseUrlString = "https://api.themoviedb.org/3"
+    private let popularFilms = "movie/popular"
+    
+}
