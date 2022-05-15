751fa4b4fd41a9bff604fed3110fba0fcf5b4bb4
Sergey Shcheglov <97shcheglov@gmail.com>
2022-05-15 18:53:55 +0600

    create custom collection cell

--- TestPanda/View/CustomCollectionViewCell.swift
+++ TestPanda/View/CustomCollectionViewCell.swift
@@ -5,4 +5,51 @@
 //  Created by Sergey Shcheglov on 13.05.2022.
 //
 
-import Foundation
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    var popularMovieVM: PopularMovieViewModel? {
+        didSet {
+            if let popularMovieVM = popularMovieVM {
+                NetworkManager.shared.getImage(urlString: "https://image.tmdb.org/t/p/original/\(popularMovieVM.posterPath)") { data in
+                    guard let data = data else {
+                        return
+                    }
+                    DispatchQueue.main.async {
+                        self.imageView.image = UIImage(data: data)
+                    }
+                }
+            }
+        }
+    }
+        
+    static let identifier = "CustomCollectionViewCell"
+    
+    let imageView: UIImageView = {
+        let image = UIImageView()
+        return image
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(imageView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        imageView.frame = CGRect(x: 0,
+                                 y: 0,
+                                 width: 90,
+                                 height: 120)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.imageView.image = nil
+    }
+}
