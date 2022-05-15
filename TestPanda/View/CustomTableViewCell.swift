ca4da6b9b08dfcd62320ce5eddbcfac0bca5b3b2
Sergey Shcheglov <97shcheglov@gmail.com>
2022-05-15 18:54:20 +0600

    change font style

--- TestPanda/View/CustomTableViewCell.swift
+++ TestPanda/View/CustomTableViewCell.swift
@@ -14,51 +14,53 @@ final class CustomTableViewCell: UITableViewCell {
                 movieTitle.text = movieVM.title
                 rating.text = String(movieVM.rating)
                 overview.text = movieVM.overview
-                NetworkManager.shared.getImage(urlString: "https://image.tmdb.org/t/p/original\(movieVM.posterPath)") { data in
-                    guard let data = data else {
-                        return
-                    }
-                    DispatchQueue.main.async {
-                        self.coverImage.image = UIImage(data: data)
-                    }
-                }
-                
+//                NetworkManager.shared.getImage(urlString: "https://image.tmdb.org/t/p/original\(movieVM.posterPath)") { data in
+//                    guard let data = data else {
+//                        return
+//                    }
+//                    DispatchQueue.main.async {
+//                        self.coverImage.image = UIImage(data: data)
+//                    }
+//                }
             }
         }
     }
     
     static let identifier = "CustomTableViewCell"
-
-    private let coverImage: UIImageView = {
+    
+    let coverImage: UIImageView = {
         let v = UIImageView()
-        v.image = UIImage(named: "mcconaughey")
+        v.contentMode = .scaleAspectFit
         return v
     }()
     
-    private let movieTitle: UILabel = {
+    let movieTitle: UILabel = {
         let v = UILabel()
         v.text = "Unpredictable"
-        v.textColor = .lightGray
+        v.font = .preferredFont(forTextStyle: .title3)
         return v
     }()
     
-    private let rating: UILabel = {
+    let rating: UILabel = {
         let v = UILabel()
         v.text = "7.8"
         v.font = UIFont.systemFont(ofSize: 16, weight: .bold)
         return v
     }()
     
-    private let overview: UILabel = {
+    let overview: UILabel = {
         let v = UILabel()
         v.text = "cool movie filmed by cool guys"
-        v.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
-        v.textColor = UIColor(red: 86/255, green: 139/255, blue: 67/255, alpha: 1)
+        v.numberOfLines = 0
+        v.lineBreakMode = .byWordWrapping
+        v.font = .preferredFont(forTextStyle: .caption1)
+        v.sizeToFit()
         return v
     }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .black
         contentView.addSubview(coverImage)
         contentView.addSubview(movieTitle)
         contentView.addSubview(rating)
@@ -73,20 +75,20 @@ final class CustomTableViewCell: UITableViewCell {
         super.layoutSubviews()
         
         coverImage.frame = CGRect(x: 0,
-                                  y: 0,
+                                  y: 4,
                                   width: 80,
                                   height: contentView.frame.size.height)
         movieTitle.frame = CGRect(x: 8 + coverImage.frame.size.width,
-                                  y: 0,
+                                  y: 4,
                                   width: contentView.frame.size.width - coverImage.frame.size.width,
                                   height: 20)
         rating.frame = CGRect(x: 8 + coverImage.frame.size.width,
                               y: 8 + movieTitle.frame.size.height ,
                               width: contentView.frame.size.width - coverImage.frame.size.width,
-                              height: 20)
+                              height: 16)
         overview.frame = CGRect(x: 8 + coverImage.frame.size.width,
                                 y: 8 + movieTitle.frame.size.height + rating.frame.size.height,
                                 width: contentView.frame.size.width - coverImage.frame.size.width,
-                                height: contentView.frame.size.height / 2)
+                                height: contentView.frame.size.height - rating.frame.size.height - movieTitle.frame.size.height - 8)
     }
 }
