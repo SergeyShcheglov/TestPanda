b04372d092ad5204231a1836fcd6879d632aea84
Sergey Shcheglov <97shcheglov@gmail.com>
2022-05-15 18:51:56 +0600

    fix urls

--- TestPanda/ViewModel/PopularMoviesViewModel.swift
+++ TestPanda/ViewModel/PopularMoviesViewModel.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct PopularMovieViewModel {
+    let popularMovie: PopularMovie
+    
+    var backdropPath: String {
+        popularMovie.backdropPath == nil
+        ? "https://t3.ftcdn.net/jpg/04/34/72/82/360_F_434728286_OWQQvAFoXZLdGHlObozsolNeuSxhpr84.jpg"
+        : "https://image.tmdb.org/t/p/original/\(popularMovie.backdropPath!)"
+    }
+    
+    var posterPath: String {
+        popularMovie.posterPath == nil
+        ? "https://t3.ftcdn.net/jpg/04/34/72/82/360_F_434728286_OWQQvAFoXZLdGHlObozsolNeuSxhpr84.jpg"
+        : "https://image.tmdb.org/t/p/original/\(popularMovie.posterPath!)"
+    }
+   
+}
